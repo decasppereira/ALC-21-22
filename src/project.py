c0a34d12ff6dd63272fa3b0a40058360b307d00e
decasppereira <43865518+decasppereira@users.noreply.github.com>
2021-10-21 18:16:41 +0100

    Started adding constraints

--- src/project.py
+++ src/project.py
@@ -1,6 +1,7 @@
 from model import *
 import sys
-
+from pysat.solvers import Glucose3
+from pysat.card import CardEnc, EncType
 
 
 class Problem:
@@ -63,7 +64,10 @@ class Problem:
         self.A = dict()
         self.translate_A = dict()
 
-    def encode(self, maxTime):
+        self.solver = Glucose3()
+        self.topLit = 0
+
+    def createVariables(self, maxTime):
         lit = 1
         for i in range(1, self.numRunners+1):
             self.X[i] = dict()
@@ -87,11 +91,49 @@ class Problem:
                 self.A[r].append(lit)
                 self.translate_A[lit] = (r, t)
                 lit +=1
+        self.topLit = lit
 
+    def encodeConstraints(self, maxTime):
+        #1 - A runner cannot spend less than 50% of the max timespan amongst other runners
+        for r in self.runners:
+            for t in range(maxTime):
+                for r2 in self.runners:         #TODO this can be moved to the loop below
+                    if(r2.id !=r.id):
+                        self.solver.add_clause([-self.A[r.id][t], self.A[r2.id][t//2]])
 
+        #2 - Runners start at time 0 and never take breaks
+        for r in self.runners:
+            lit = self.A[r.id][0]
+            self.solver.add_clause([lit])
+            for t in range(1, maxTime-1):       #TODO olhar pa isto c atencao
+                self.solver.add_clause([-self.A[r.id][t], self.A[r.id][t-1]]) 
+                self.solver.add_clause([self.A[r.id][t], -self.A[r.id][t+1]])
+
+        #3 - All products from all orders must arrive to the packaging area
+        for p in self.products:
+            qty = self.productInventory[p.id]
+            enc = CardEnc.equals(self.P[p.id], bound = qty, top_id=self.topLit, encoding=EncType.pairwise)
+            for clause in enc.clauses:
+                self.solver.add_clause(clause)
+
+        #4 - Only one product arriving to the packaging area at a time
+        for k in range(0, maxTime):
+            literals = [p[k] for p in self.P.values()]
+            enc = CardEnc.atmost(literals, bound = 1, top_id=self.topLit, encoding=EncType.pairwise)
+            for clause in enc.clauses:
+                self.solver.add_clause(clause)
+                
+        #5 - A runner takes t_ij time from product i to product j.
+
+        #6 - A product takes c_j time from the conveyor belt to the packaging area
+
+        #7 - A runner can only carry one product at a time
             
 
 
 if __name__ == '__main__':
     p = Problem(sys.stdin.readlines())
-    p.encode(3)
+    
+    timebound = 3
+    p.createVariables(timebound)
+    p.encodeConstraints(timebound)
